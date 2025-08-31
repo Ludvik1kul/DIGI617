08dbbbc3e07e3249c23ab3e4bd129806bc505d32
Ludvik Slipers <lsl007@uib.no>
2025-08-31 23:47:45 +0200

    test

--- app/access.php
+++ app/access.php
@@ -1,8 +1,7 @@
 <?php
 if (isset($_GET['text'])) {
     $input = escapeshellarg($_GET['text']);  // safely escape input
-    $command = "python3 model.py " . $input; // run Python
-    $output = shell_exec($command);          // capture result
+    $output = shell_exec("python3 /app/model.py " . $input);
     echo $output;                            // return it
 } else {
     echo "No input provided";
