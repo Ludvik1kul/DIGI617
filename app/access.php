18fa68f83b2d27c747f29b41c4eb59b8f2f9d940
Ludvik Slipers <lsl007@uib.no>
2025-09-02 20:55:59 +0200

    restruct

--- app/access.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['text'])) {
-    $input = escapeshellarg($_GET['text']);  // safely escape input
-    $output = shell_exec("python3 /app/model.py " . $input);
-    echo $output;                            // return it
-} else {
-    echo "No input provided";
-}
-?>
