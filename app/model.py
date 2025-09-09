4e2c553fbd389a25d884bc285340c4256de20974
Ludvik Slipers <lsl007@uib.no>
2025-09-09 13:24:42 +0200

    norsk model

--- app/model.py
+++ app/model.py
@@ -1,8 +1,15 @@
 import sys
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+import pandas as pd
+tokenizer = AutoTokenizer.from_pretrained("Kushtrim/norbert3-large-norsk-sentiment-sst2", trust_remote_code=True)
 
-def main():
-    input=sys.argv[1]
-    return input+"Skibidi"
+model = AutoModelForSequenceClassification.from_pretrained("Kushtrim/norbert3-large-norsk-sentiment-sst2", trust_remote_code=True, torch_dtype="auto")
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+def main(text="Jeg er glad"):
+
+    output=classifier(text)
+    return output
 
 if __name__ == "__main__":
     print(main())
